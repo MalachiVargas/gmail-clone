32d51ad4128c47c08840d7566c0699f307122971
Malachi Vargas <mcvarga7@gmail.com>
2021-12-16 23:43:08 -0500

    Built Mail Screen

---
+++ src/features/mailSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+
+export const mailSlice = createSlice({
+  name: 'mail',
+  initialState: {
+    sendMessageIsOpen: false,
+  },
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    openSendMessage: state => {
+      state.sendMessageIsOpen = true;
+    },
+    closeSendMessage: state => {
+      state.sendMessageIsOpen = false;
+    },
+  },
+});
+
+export const { openSendMessage, closeSendMessage } = mailSlice.actions;
+
+export const selectMail = state => state.mail.value;
+
+export default mailSlice.reducer;
